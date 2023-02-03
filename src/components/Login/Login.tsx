ba30389dc5c86571d2d723f5d6e7fa857b4e0bac
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-02-03 13:55:03 -0500

    install: tailwind and other dependencies

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,11 @@
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div id='login' className='mr-auto ml-auto mt-[5rem] w-[50%] p-[2rem] rounded-sm'>
+        <h1>RythmHacks Application Portal</h1>
+    </div>
+  )
+}
+
+export default Login
