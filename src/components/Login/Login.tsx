8937cef27008322c9b73afd10d69c30f0d3f6be3
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-02-05 14:39:03 -0500

    backend: init supabase

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,14 +1,63 @@
 import './Login.scss'
+import { useState } from 'react'
+import { supabase } from '../../supabaseClient'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
+
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault()
+
+    try {
+      setLoading(true)
+      const { error } = await supabase.auth.signInWithOtp({ email })
+      if (error) throw error
+      alert('Check your email for the login link!')
+    } catch (error: any) {
+      alert(error.error_description || error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <div id='login' className='mr-auto ml-auto mt-[5rem]'>
-        <p className='uppercase text-[#888] desc'>log in</p>
-        <h1 className=''>Log In to RythmHacks</h1>
+        <p className='uppercase text-[#888] desc m-0'>log in</p>
+        <div className='flex justify-between items-center'>
+          <h1 className=''>Log In to RythmHacks</h1>
+          <img src={logo} alt='loginlogo' className='rounded-full h-[4rem]'></img>
+        </div>
 
         <form>
           
         </form>
+        <div className="row flex-center flex">
+        <div className="col-6 form-widget" aria-live="polite">
+          <h1 className="header">Supabase + React</h1>
+          <p className="description">Sign in via magic link with your email below</p>
+          {loading ? (
+            'Sending magic link...'
+          ) : (
+            <form onSubmit={handleLogin}>
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                className="inputField"
+                type="email"
+                placeholder="Your email"
+                value={email}
+                onChange={(e:any) => setEmail(e.target.value)}
+              />
+              <button className="button block" aria-live="polite">
+                Send magic link
+              </button>
+            </form>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
