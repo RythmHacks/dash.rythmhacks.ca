7e5b1a08925620344ebdd6e54ee52d777650287b
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-02-05 13:16:26 -0500

    fix: metadata

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,8 +2,13 @@ import './Login.scss'
 
 const Login = () => {
   return (
-    <div id='login' className='mr-auto ml-auto mt-[5rem] w-[40%]'>
-        <h1>RythmHacks Application Portal</h1>
+    <div id='login' className='mr-auto ml-auto mt-[5rem]'>
+        <p className='uppercase text-[#888] desc'>log in</p>
+        <h1 className=''>Log In to RythmHacks</h1>
+
+        <form>
+          
+        </form>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#558CA9"
+      }
+    },
   },
   plugins: [],
 }
