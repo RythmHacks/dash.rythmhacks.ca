a7d277d199188aa6e393c4670ff6693b75921051
Li Feng Yin <lifeng-yin@proton.me>
2023-02-11 12:26:51 -0500

    Style log in page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,7 +4,9 @@ import { useAuth } from '../../contexts/Auth'
 import logo from '../../assets/logo.png'
 
 const Login = () => {
-  const { signInWithOtp } = useAuth()
+  const { signInWithOtp, user } = useAuth()
+  console.log(user)
+
 
   const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState('')
@@ -29,31 +31,27 @@ const Login = () => {
     <div id='login' className='mr-auto ml-auto mt-[5rem]'>
         <p className='uppercase text-[#888] desc m-0'>log in</p>
         <div className='flex justify-between items-center'>
-          <h1 className=''>Log In to RythmHacks</h1>
+          <h1 className='leading-normal'>Log In to RythmHacks</h1>
           <img src={logo} alt='loginlogo' className='rounded-full h-[4rem]'></img>
         </div>
 
-        <form>
-          
-        </form>
         <div className="row flex-center flex">
         <div className="col-6 form-widget" aria-live="polite">
-          <h1 className="header">Supabase + React</h1>
-          <p className="description">Sign in via magic link with your email below</p>
+          <p className="description">Sign in via magic link with your email below:</p>
           {loading ? (
             'Sending magic link...'
           ) : (
             <form onSubmit={handleLogin}>
-              <label htmlFor="email">Email</label>
               <input
                 id="email"
-                className="inputField"
+                className="inputField border-style-none bg-gray-100 border-solid border-2 border-gray-100 rounded w-full py-2 px-4 mb-4 text-gray-700 focus:outline-none focus:bg-white focus:border-accent"
                 type="email"
                 placeholder="Your email"
                 value={email}
                 onChange={(e:any) => setEmail(e.target.value)}
+                required
               />
-              <button className="button block" aria-live="polite">
+              <button className="button block border-none rounded-md text-white text-lg font-sans bg-accent  px-3 py-1" aria-live="polite">
                 Send magic link
               </button>
             </form>
