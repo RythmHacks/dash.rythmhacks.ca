a7ab536beb76d11b93c158bd412188047b9db62c
Jeffrey Zang <66485719+jeffrey-zang@users.noreply.github.com>
2023-03-10 17:22:27 -0500

    feat(front): convert to dark mode, ui rework (#5)
    * feat(front): dark mode and colour palette
    
    * feat(front): finish restyling
    
    * refac(front): reword some dialogs

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { useAuth } from '../../contexts/Auth'
-import logo from '../../assets/logo.png'
+import logo from '../../assets/rythmhacks-circle.png'
+import { BsCheckCircle } from 'react-icons/bs'
 
 const Login = () => {
   const { signInWithOtp } = useAuth()
 
   const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState('')
+  const [sent, setSent] = useState(false)
 
   const handleLogin = async (e: any) => {
     e.preventDefault()
@@ -15,7 +17,8 @@ const Login = () => {
       setLoading(true)
       const { error } = await signInWithOtp({ email })
       if (error) throw error
-      alert('Check your email for the login link!')
+      alert(`Check your email (${email}) for the login link!`)
+      setSent(true)
     } catch (error: any) {
       alert(error.error_description || error.message)
       console.error(error)
@@ -25,32 +28,33 @@ const Login = () => {
   }
 
   return (
-    <div id='login' className='w-5/12 p-8 mr-auto ml-auto mt-[5rem] rounded-lg border-2 border-solid border-neutral-100'>
-        <p className='uppercase text-[#888] desc m-0'>log in</p>
-        <div className='flex justify-between items-center'>
-          <h1 className='leading-normal'>Log In to RythmHacks</h1>
-          <img src={logo} alt='loginlogo' className='rounded-full h-[4rem]'></img>
+    <div id='login' className='w-5/12 p-10 mr-auto ml-auto mt-[5rem] rounded-lg bg-[#121313] min-w-[330px]'>
+        <p className='uppercase text-[#888] text-[0.8rem] m-0'>log in</p>
+        <div className='flex justify-between items-center gap-4'>
+          <h1 className='mt-4'>Log In to RythmHacks</h1>
+          <img src={logo} alt='loginlogo' className='rounded-md h-[4rem]'></img>
         </div>
 
-        <div className="row flex-center flex">
-        <div className="col-6 form-widget" aria-live="polite">
-          <p className="description">Sign in via magic link with your email below:</p>
+        <div className="row flex-center flex w-full">
+        <div className="col-6 form-widget w-full" aria-live="polite">
+          <p className="text-[#bbb]">Enter your email to get a magic link</p>
           {loading ? (
             'Sending magic link...'
           ) : (
             <form onSubmit={handleLogin}>
               <input
                 id="email"
-                className="inputField border-style-none bg-gray-100 border-solid border-2 border-gray-100 rounded w-full py-2 px-4 mb-4 text-gray-700 focus:outline-none focus:bg-white focus:border-accent"
+                className="mb-4 px-4 py-2 w-full"
                 type="email"
-                placeholder="Your email"
+                placeholder="email@example.com"
                 value={email}
                 onChange={(e:any) => setEmail(e.target.value)}
                 required
               />
-              <button className="button block border-none rounded-md text-white text-lg font-sans bg-accent  px-3 py-1" aria-live="polite">
+              <button className='w-full text-white'>
                 Send magic link
               </button>
+              {(sent) ? <p className='items-center flex gap-2 text-[#bbb]'><BsCheckCircle/> Magic link sent successfully, you can now close this window</p> : <p></p>}
             </form>
           )}
         </div>
