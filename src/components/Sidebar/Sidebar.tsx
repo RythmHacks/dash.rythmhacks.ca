6e75c82755b19a4e23692ecb8d471791b825ddcc
Li Feng Yin <lifeng-yin@proton.me>
2023-03-16 21:40:57 -0400

    Feat(front): Add icons to popup

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,8 +4,11 @@ import logo from '../../assets/rythmhacks-circle.png'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { BiHome } from 'react-icons/bi'
 import { BsClipboard2Check } from 'react-icons/bs'
+import { IoMdSettings } from 'react-icons/io'
+import { IoMdLogOut } from 'react-icons/io'
 import { RxPerson } from 'react-icons/rx'
 import { useAuth } from '../../contexts/Auth'
+
 const Sidebar = () => {
     const { user, signOut } = useAuth();
     let location = useLocation().pathname
@@ -44,10 +47,12 @@ const Sidebar = () => {
 
             <div>
                 <div className="account-popup flex-col" style={{ display: accountPopupOpened ? "flex" : "none" }}>
-                    <div className="w-full h-12 p-4 hover:bg-dark2">
+                    <div className="w-full h-12 p-4 flex items-center gap-2 hover:bg-dark2">
+                        <IoMdSettings size={24}/>
                         <Link to="/dashboard/settings" className={(location === '/dashboard/apply') ? "active" : ""}>Settings</Link>
                     </div>
-                    <div className="w-full h-12 p-4 hover:bg-dark2">
+                    <div className="w-full h-12 p-4 flex items-center gap-2 hover:bg-dark2">
+                        <IoMdLogOut size={24}/>
                         <button className="style-none" onClick={() => logout()}>Logout</button>
                     </div>
                 </div>
