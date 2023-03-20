8f3db2454b071f140b7bf71798e8a9bc251bf76a
Li Feng Yin <lifeng-yin@proton.me>
2023-03-20 18:47:34 -0400

    refac(front): refac user auth logic from app.tsx to new dashboard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import { supabase } from './supabaseClient';
 import Login from './components/Login/Login'
 import Apply from './components/Apply/Apply'
 import Home from './components/Home/Home'
-import Sidebar from './components/Sidebar/Sidebar'
 import Settings from './components/Settings/Settings'
+import Dashboard from './components/Dashboard/Dashboard';
 
 const App = () => {
   const { user } = useAuth()
@@ -17,21 +17,16 @@ const App = () => {
 
   return (
     <div className='flex'>
-      <Sidebar />
       <Routes>
         <Route path='/' element = {(user === null) ? <Navigate to='/login' /> : <Navigate to='/dashboard' />} />
         <Route path="/login" element={(user === null) ? <Login /> : <Navigate to='/dashboard' />} />
-      </Routes>
-      {(user === null) ? <div>{(location.pathname !== '/login') ?  <div>You're not logged in! Click <Link to='/login'>here</Link> to be redirected to the login page.</div> : null}</div> : 
-      <Routes>
-        <Route path='/dashboard'>
+        <Route path='/dashboard' element={<Dashboard />}>
           <Route index element={<Home />} />
           <Route path="apply" element={<Apply />} />
           <Route path="settings" element={<Settings />} />
         </Route>
         <Route path="*" element={<div>404</div>} />
       </Routes>
-      }
     </div>
   )
 }
