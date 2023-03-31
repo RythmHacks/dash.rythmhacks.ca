60c94f7922f6b27372ad5e4415f208d3c2f021c9
Li Feng Yin <lifeng-yin@proton.me>
2023-03-30 20:51:19 -0400

    feat(front): add application questions editor (#16)
    * feat(front): install lexical
    
    * feat(front): add editor component using lexical
    
    * feat(front): add prototype, unstyled application status
    
    * feat(front): add bold text support for inter
    
    * refac(front): remove rich text strikethrough button
    
    * refac: restyle rich text toolbar
    
    ---------
    
    Co-authored-by: Jeffrey Zang <zangj4548@wrdsb.ca>

--- src/components/Apply/Apply.tsx
+++ src/components/Apply/Apply.tsx
@@ -1,6 +1,25 @@
+import { useState } from "react"
+import Editor from "../Editor/Editor"
+
 const Apply = () => {
+  const [editingInProgress, setEditingInProgress] = useState(false)
+
   return (
-    <div>Ready to begin your application?</div>
+    <div className="p-12 flex-1" id="apply">
+      { !editingInProgress ? 
+        <div className="container">
+          <div className="flex flex-col">
+            <p>Application Status:</p>
+            <p className="text-2xl text-orange-600">Not Submitted</p>
+          </div>
+          <button onClick={() => setEditingInProgress(true)}>Edit application</button>
+        </div>
+      :
+        <>
+          <Editor />
+        </>
+      }
+    </div>
   )
 }
 
