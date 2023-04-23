248a74caaef827d2f484cc4a6f7f2adf8ff593b3
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-04-23 17:04:03 -0400

    chore: cleanup application dashboard page

--- src/pages/Apply/Apply.tsx
+++ src/pages/Apply/Apply.tsx
@@ -7,12 +7,26 @@ const Apply = () => {
   return (
     <div className="p-12 flex-1" id="apply">
       { !editingInProgress ? 
-        <div className="container">
-          <div className="flex flex-col">
-            <p>Application Status:</p>
-            <p className="text-2xl text-orange-600">Not Submitted</p>
+        <div>
+          <div className='container mb-4'>
+            <h1>Application Dashboard</h1>
+            <p>This is the hub for your RythmHacks application. Fill out your application, get a response from us, then RSVP for the event.</p>
           </div>
-          <button onClick={() => setEditingInProgress(true)}>Edit application</button>
+          <div className='flex gap-4'>
+            <div className="container w-1/2">
+              <h2 className='flex items-center justify-between'>
+                Current Status
+                <p className="text-xl text-gray-500 bg-black p-2 rounded-md flex items-center font-normal">Not started</p>
+              </h2>
+              <p className='mt-4'>You haven't started your application! Click the button below to start filling it out.</p>
+              <button onClick={() => setEditingInProgress(true)} className='mt-8'>Begin application</button>
+            </div>
+            <div className='container w-1/2'>
+              <h2>More info</h2>
+              <p className='font-bold'>When are applications due?</p>
+              <p>Applications are due on July 31st, 2023.</p>
+            </div>
+          </div>
         </div>
       :
         <>
