65e210829d08ef56dfa23f2a3074d1de69a7a64c
Li Feng Yin <lifeng-yin@proton.me>
2023-04-30 16:09:58 -0400

    feat(front): add input event handler, add name attribute to form elements

--- src/pages/Apply/Apply.tsx
+++ src/pages/Apply/Apply.tsx
@@ -1,20 +1,30 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Editor from "../../components/Editor/Editor"
 import { Link } from 'react-router-dom'
 
 const Apply = () => {
   const [editingInProgress, setEditingInProgress] = useState(false)
 
-  const [formData, setFormData] = useState({
+  const [applicationData, setApplicationData] = useState({
+    gender: '',
     age: '',
     grade: '',
     phone_number: '',
     school: '',
-    gender: '',
-    t_shirt_size: ''
-   
- 
- 
+
+    t_shirt_size: 'Not Selected',
+    dietary_restrictions: [],
+
+    country: '',
+    province: '',
+    address: '',
+    apartment_suite: '',
+    postal_code: '',
+
+    question_1: '',
+    question_2: '',
+
+    mlh_newsletter: false
   })
  
  
@@ -28,8 +38,19 @@ const Apply = () => {
  
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault()
-    alert(formData)
+    console.log(applicationData)
   }
+
+  const handleInput = (event: Event) => {
+    console.log('Input created')
+    console.log(event)
+  }
+
+  useEffect(() => {
+    if (editingInProgress) document.addEventListener('input', handleInput)
+
+    return () => document.removeEventListener('input', handleInput)
+  }, [editingInProgress])
  
   return (
     <div className="p-12 flex-1" id="apply">
@@ -65,11 +86,11 @@ const Apply = () => {
           <p>Fill out this form to register for the event as a hacker. <Link to='/dashboard/apply'>Go back to the dashboard.</Link></p>
         </div>
         <div className='container mt-4'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <h3>Basic Information</h3>
             <div>
               <label htmlFor="gender">Gender (optional)</label>
-              <select id="gender">
+              <select id="gender" name="gender">
                 <option>Prefer not to say</option>
                 <option>Male</option>
                 <option>Female</option>
@@ -80,13 +101,13 @@ const Apply = () => {
 
             <div>
               <label htmlFor="age">Age</label>
-              <input id="age" type="number" min={0} max={100} />
+              <input id="age" name="age" type="number" min={0} max={100} />
             </div>
 
 
             <div>
               <label htmlFor="grade">Grade (as of September 2023)</label>
-              <select id="grade">
+              <select id="grade" name="grade">
                 <option value="">Please select a grade</option>
                 <option>10</option>
                 <option>11</option>
@@ -98,19 +119,20 @@ const Apply = () => {
 
             <div>
               <label htmlFor="phone-number">Phone Number</label>
-              <input id="phone-number" type="phone-number" />
+              <input id="phone-number" name="phone_number" />
             </div>
 
 
             <div>
               <label htmlFor="school">School</label>
-              <input id="school" type="text" />
+              <input id="school" name="school" />
             </div>
           
             <h3>Event-specific Information</h3>
             <div>
-              <label htmlFor="gender">T-Shirt Size (optional)</label>
-              <select id="gender">
+              <label htmlFor="t-shirt-size">T-Shirt Size (optional)</label>
+              <select id="t-shirt-size" name="t_shirt_size">
+                <option>Not Selected</option>
                 <option>XS</option>
                 <option>S</option>
                 <option>M</option>
@@ -122,15 +144,13 @@ const Apply = () => {
 
             <div>
               <label>Dietary Restrictions</label>
-              <br></br>
-
 
               { dietaryRestrictions.map((d) => (<>
-                <input type="checkbox" id={d} value={d} />
+                <input type="checkbox" id={d} name="dietary_restrictions" value={d} />
                 <label htmlFor={d} className="capitalize">{d}</label>
               </>)) }
             
-              <input type="checkbox" id="other" value="other"></input>
+              <input type="checkbox" id="other" name="dietary_restrictions" value="other"></input>
               <label htmlFor="other">Other</label>
             </div>
 
@@ -169,20 +189,27 @@ const Apply = () => {
               <input id="address" placeholder="10 Main Street" />
             </div>
 
+            <div>
+              <label htmlFor="apartment_suite">Apartment, Suite, etc. (optional)</label>
+              <input id="apartment_suite" placeholder="Unit 4" />
+            </div>
+
 
             <div>
               <label htmlFor="postal-code">Postal Code</label>
               <input id="postal-code" placeholder="N2V 3Q8" />
             </div>
-          </form>
+          
 
-          <h3>Application Questions</h3>
-          <Editor />
-          <Editor />
-          <div className='flex gap-2 mt-4'>
-            <button className='contrast' onClick={() => setEditingInProgress(false)}>Return to dashboard</button>
-            <button>Submit</button>
-          </div>
+            <h3>Application Questions</h3>
+            <Editor />
+            <Editor />
+            
+            <div className='flex gap-2 mt-4'>
+              <button className='contrast' onClick={() => setEditingInProgress(false)}>Return to dashboard</button>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
         </div>
         </>
       }
