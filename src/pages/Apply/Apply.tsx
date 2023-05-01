789a8a3831355bd6e40d3cf10e65699e9edfdf58
Li Feng Yin <lifeng-yin@proton.me>
2023-05-01 19:05:42 -0400

    feat(front): finalize html for fields, add other options, keep track of application using state

--- src/pages/Apply/Apply.tsx
+++ src/pages/Apply/Apply.tsx
@@ -1,56 +1,65 @@
 import { useState, useEffect } from "react"
 import Editor from "../../components/Editor/Editor"
 import { Link } from 'react-router-dom'
+import { useAuth } from "../../contexts/Auth"
 
 const Apply = () => {
+  const { supabase } = useAuth()
+
   const [editingInProgress, setEditingInProgress] = useState(false)
 
   const [applicationData, setApplicationData] = useState({
-    gender: '',
+    gender: 'Prefer not to say',
     age: '',
     grade: '',
     phone_number: '',
     school: '',
 
     t_shirt_size: 'Not Selected',
-    dietary_restrictions: [],
+    dietary_restrictions_vegetarian: false,
+    dietary_restrictions_gluten_free: false,
+    dietary_restrictions_dairy_free: false,
+    dietary_restrictions_halal: false,
+    dietary_restrictions_other: '',
 
-    country: '',
+    country: 'Canada',
     province: '',
+    city: '',
     address: '',
     apartment_suite: '',
     postal_code: '',
 
     question_1: '',
     question_2: '',
-
-    mlh_newsletter: false
   })
- 
- 
-  const dietaryRestrictions = [
-    "vegetarian",
-    "gluten-free",
-    "lactose-free",
-    "halal",
-  ]
+
+  const updateApplicationData = (column: string, value: string | boolean) => {
+    setApplicationData(appData => ({
+      ...appData,
+      [column]: value,
+    }))
+  }
+
+  const [isOtherDietaryRestrictionChecked, setIsOtherDietaryRestrictionChecked] = useState(false)
  
  
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault()
-    console.log(applicationData)
+    
   }
 
-  const handleInput = (event: Event) => {
-    console.log('Input created')
-    console.log(event)
-  }
-
-  useEffect(() => {
-    if (editingInProgress) document.addEventListener('input', handleInput)
-
-    return () => document.removeEventListener('input', handleInput)
-  }, [editingInProgress])
+  /*useEffect(() => {
+    supabase.from('applications-hackers').select('*')
+      .then(({ data, error }) => {
+        if (data === null || !data.length || error) {
+          alert('Oh no! Your data could not be retrieved. If this error persists, contact the RythmHacks team.')
+          if (error) throw error;
+          else throw TypeError('no application matching the id was found')
+        }
+        
+        setApplicationData(data[0])
+      })
+  }, [supabase])*/
  
   return (
     <div className="p-12 flex-1" id="apply">
@@ -90,24 +99,48 @@ const Apply = () => {
             <h3>Basic Information</h3>
             <div>
               <label htmlFor="gender">Gender (optional)</label>
-              <select id="gender" name="gender">
+              <select
+                id="gender"
+                value={["Prefer not to say", "Male", "Female", "Non-binary"].includes(applicationData.gender) ? applicationData.gender : ''}
+                onChange={e => {
+                  updateApplicationData('gender', e.target.value)
+                }}
+                >
                 <option>Prefer not to say</option>
                 <option>Male</option>
                 <option>Female</option>
-                <option>Other</option>
+                <option>Non-binary</option>
+                <option value="">Prefer to self-describe</option>
               </select>
+              {!["Prefer not to say", "Male", "Female", "Non-binary"].includes(applicationData.gender) && (<>
+                <label htmlFor="self-described-gender">Self-described Gender:</label>
+                <input id="self-described-gender" value={applicationData.gender} onChange={e => {
+                  updateApplicationData('gender', e.target.value)
+                }}></input>
+              </>)}
             </div>
 
 
             <div>
-              <label htmlFor="age">Age</label>
-              <input id="age" name="age" type="number" min={0} max={100} />
+              <label htmlFor="age">Age (as of September 1, 2023)</label>
+              <input
+                id="age"
+                type="number"
+                min={0}
+                max={100}
+                value={applicationData.age}
+                onChange={e => updateApplicationData('age', e.target.value)}
+              />
             </div>
 
 
             <div>
-              <label htmlFor="grade">Grade (as of September 2023)</label>
-              <select id="grade" name="grade">
+              <label htmlFor="grade">Grade (as of September 1, 2023)</label>
+              <select
+                id="grade"
+                value={applicationData.grade}
+                onChange={e => updateApplicationData('grade', e.target.value)}
+              >
                 <option value="">Please select a grade</option>
                 <option>10</option>
                 <option>11</option>
@@ -118,20 +151,35 @@ const Apply = () => {
 
 
             <div>
-              <label htmlFor="phone-number">Phone Number</label>
-              <input id="phone-number" name="phone_number" />
+              <label htmlFor="school">School</label>
+              <input 
+                id="school"
+                value={applicationData.school}
+                onChange={e => updateApplicationData('school', e.target.value)}
+               />
             </div>
 
 
             <div>
-              <label htmlFor="school">School</label>
-              <input id="school" name="school" />
+              <label htmlFor="phone-number">Phone Number</label>
+              <input
+                id="phone-number"
+                type="tel"
+                required
+                value={applicationData.phone_number}
+                onChange={e => updateApplicationData('phone_number', e.target.value)}
+              ></input>
             </div>
+
           
             <h3>Event-specific Information</h3>
             <div>
               <label htmlFor="t-shirt-size">T-Shirt Size (optional)</label>
-              <select id="t-shirt-size" name="t_shirt_size">
+              <select 
+                id="t-shirt-size"
+                value={applicationData.t_shirt_size}
+                onChange={e => updateApplicationData('t_shirt_size', e.target.value)}
+              >
                 <option>Not Selected</option>
                 <option>XS</option>
                 <option>S</option>
@@ -144,14 +192,62 @@ const Apply = () => {
 
             <div>
               <label>Dietary Restrictions</label>
+              
+              <div>
+                <input 
+                  type="checkbox"
+                  id="dietary-restrictions-vegetarian"
+                  checked={applicationData.dietary_restrictions_vegetarian}
+                  onChange={e => updateApplicationData('dietary_restrictions_vegetarian', e.target.checked)} />
+                <label htmlFor="dietary-restrictions-vegatarian" className="capitalize">Vegetarian</label>
+              </div>
 
-              { dietaryRestrictions.map((d) => (<>
-                <input type="checkbox" id={d} name="dietary_restrictions" value={d} />
-                <label htmlFor={d} className="capitalize">{d}</label>
-              </>)) }
-            
-              <input type="checkbox" id="other" name="dietary_restrictions" value="other"></input>
-              <label htmlFor="other">Other</label>
+              <div>
+                <input 
+                  type="checkbox"
+                  id="dietary-restrictions-halal"
+                  checked={applicationData.dietary_restrictions_halal}
+                  onChange={e => updateApplicationData('dietary_restrictions_halal', e.target.checked)} />
+                <label htmlFor="dietary-restrictions-halal" className="capitalize">Halal</label>
+              </div>
+
+              <div>
+                <input 
+                  type="checkbox"
+                  id="dietary-restrictions-gluten-free"
+                  checked={applicationData.dietary_restrictions_gluten_free}
+                  onChange={e => updateApplicationData('dietary_restrictions_gluten_free', e.target.checked)} />
+                <label htmlFor="dietary-restrictions-gluten-free" className="capitalize">Gluten-Free</label>
+              </div>
+
+              <div>
+                <input 
+                  type="checkbox"
+                  id="dietary-restrictions-dairy_free"
+                  checked={applicationData.dietary_restrictions_dairy_free}
+                  onChange={e => updateApplicationData('dietary_restrictions_dairy_free', e.target.checked)} />
+                <label htmlFor="dietary-restrictions-dairy-free" className="capitalize">Dairy-Free</label>
+              </div>
+
+
+              <div>
+                <input
+                  type="checkbox"
+                  id="other-dietary-restrictions-checkbox"
+                  name="dietary_restrictions"
+                  value={applicationData.dietary_restrictions_other}
+                  onChange={() => setIsOtherDietaryRestrictionChecked(!isOtherDietaryRestrictionChecked)}
+                ></input>
+                <label htmlFor="other-dietary-restrictions-checkbox">Other restriction(s)</label>
+              </div>
+
+              
+              {isOtherDietaryRestrictionChecked && (<>
+                <label htmlFor="other-dietary-restrictions-input">Restriction(s):</label>
+                <input id="other-dietary-restrictions-input" value={applicationData.dietary_restrictions_other} onChange={e => {
+                    updateApplicationData('dietary_restrictions_other', e.target.value)
+                  }}></input>
+              </>)}
             </div>
 
 
@@ -159,52 +255,101 @@ const Apply = () => {
             <p>These fields are optional if you want to recieve prizes.</p>
 
             <div>
-              <input type="radio" name="country" id="canada" value="Canada" defaultChecked></input>
+              <input 
+                type="radio"
+                name="country"
+                id="canada"
+                checked={applicationData.country === 'Canada'}
+                onChange={() => { updateApplicationData('country', 'Canada'); console.log(applicationData);}}
+              ></input>
               <label htmlFor="canada">Canada</label>
 
 
-              <input type="radio" name="country" id="usa" value="United States"></input>
+              <input
+                type="radio"
+                name="country"
+                id="usa"
+                checked={applicationData.country === 'United States of America'}
+                onChange={() => updateApplicationData('country', 'United States of America')}
+              ></input>
               <label htmlFor="usa">United States</label>
 
 
-              <input type="radio" name="country" id="other"></input>
-              <label htmlFor="other">Other</label>
+              <input
+                type="radio"
+                name="country"
+                id="other-country-checkbox"
+                checked={applicationData.country !== 'Canada' && applicationData.country !== 'United States of America'}
+                onChange={() => updateApplicationData('country', '')}></input>
+              <label htmlFor="other-country-checkbox">Other</label>
+
+              {applicationData.country !== 'Canada' && applicationData.country !== 'United States of America' && (<>
+                <label htmlFor="other-country-input">Country (No abbrevations):</label>
+                <input id="other-country-input" value={applicationData.country} onChange={e => {
+                    updateApplicationData('country', e.target.value)
+                  }}></input>
+              </>)}
             </div>
 
 
             <div>
               <label htmlFor="province">Province</label>
-              <input id="province" placeholder="Ontario" />
+              <input 
+                id="province"
+                placeholder="Ontario"
+                value={applicationData.province}
+                onChange={e => updateApplicationData('province', e.target.value)}
+              />
             </div>
 
 
             <div>
               <label htmlFor="city">City</label>
-              <input id="city" placeholder="Waterloo" />
+              <input
+                id="city"
+                placeholder="Waterloo"
+                value={applicationData.city}
+                onChange={e => updateApplicationData('city', e.target.value)}
+              />
             </div>
 
 
             <div>
               <label htmlFor="address">Address</label>
-              <input id="address" placeholder="10 Main Street" />
+              <input
+                id="address"
+                placeholder="10 Main Street"
+                value={applicationData.address}
+                onChange={e => updateApplicationData('address', e.target.value)}
+              />
             </div>
 
             <div>
               <label htmlFor="apartment_suite">Apartment, Suite, etc. (optional)</label>
-              <input id="apartment_suite" placeholder="Unit 4" />
+              <input
+                id="apartment_suite"
+                placeholder="Unit 4"
+                value={applicationData.apartment_suite}
+                onChange={e => updateApplicationData('apartment_suite', e.target.value)}
+              />
             </div>
 
 
             <div>
               <label htmlFor="postal-code">Postal Code</label>
-              <input id="postal-code" placeholder="N2V 3Q8" />
+              <input
+                id="postal-code"
+                placeholder="N2V 3Q8"
+                value={applicationData.postal_code}
+                onChange={e => updateApplicationData('postal_code', e.target.value)}
+              />
             </div>
           
 
             <h3>Application Questions</h3>
             <Editor />
             <Editor />
-            
+
             <div className='flex gap-2 mt-4'>
               <button className='contrast' onClick={() => setEditingInProgress(false)}>Return to dashboard</button>
               <button type="submit">Submit</button>
