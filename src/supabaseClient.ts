962ae39bbddc26239640c4064df3884d20751a4d
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-05-21 09:39:30 -0400

    feat(front): migrate from CRA to vite

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from './types/supabase';
 
-const supabaseUrl:string = process.env.REACT_APP_SUPABASE_URL!;
-const supabaseAnonKey:string = process.env.REACT_APP_SUPABASE_ANON_KEY!;
+const supabaseUrl:string = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey:string = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
