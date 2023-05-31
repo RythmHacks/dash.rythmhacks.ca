a53fb5ea771390fbf43f52853f31d28a4d696371
Li Feng Yin <lifeng-yin@proton.me>
2023-05-31 01:19:47 +0000

    fix(front): use origin instead of base_url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-  }
-});
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''))
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      open: true,
+      origin: process.env.ORIGIN
+    }
+  })
+}
