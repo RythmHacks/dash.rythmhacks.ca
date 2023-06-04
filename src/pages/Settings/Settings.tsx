6bd5f0349e62fa43b50ed8e05dfd97e56fd9d397
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-06-04 15:12:13 -0400

    fix(auth): add section about deleting accounts

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -42,83 +42,83 @@ const Settings = () => {
       }
     }
 
-    // const deleteUser = async () => {
-    //   const { data, error } = await supabase.auth.admin.deleteUser(
-    //     user?.id!
-    //   )
-    // }
-
     return (
       <div className="page" id="settings">
-        <div className="container">
-          <form onSubmit={handleUserInfoSubmit}>
-            <h1 className="mb-4">User info</h1>
-            <div className="flex gap-4 lg:flex-row flex-col">
+        <div className='flexwrap'>
+          <div className="container">
+            <form onSubmit={handleUserInfoSubmit}>
+              <h1 className="mb-4">User info</h1>
+              <div className="flex gap-4 lg:flex-row flex-col">
+                <div className="mb-4">
+                  <label className="block mb-2" htmlFor="firstname">First Name</label>
+                  <input 
+                    className="p-2 text-md w-full" 
+                    id="firstname" 
+                    type="text"
+                    required
+                    value={firstName}
+                    onChange={e => {
+                      setFirstName(e.target.value)
+                    }}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block mb-2" htmlFor="lastname">Last Name</label>
+                  <input 
+                    className="p-2 text-md w-full" 
+                    id="lastname" 
+                    type="text" 
+                    required
+                    value={lastName}
+                    onChange={e => {
+                      setLastName(e.target.value)
+                    }}
+                  />
+                </div>
+              </div>
               <div className="mb-4">
-                <label className="block mb-2" htmlFor="firstname">First Name</label>
-                <input 
-                  className="p-2 text-md w-full" 
-                  id="firstname" 
-                  type="text"
+                <label className="block mb-2" htmlFor="email">Email</label>
+                <input
+                  className="w-full p-2 text-md"
+                  id="email"
+                  type="email"
                   required
-                  value={firstName}
+                  value={email}
                   onChange={e => {
-                    setFirstName(e.target.value)
+                    setEmail(e.target.value)
                   }}
                 />
               </div>
-              <div className="mb-4">
-                <label className="block mb-2" htmlFor="lastname">Last Name</label>
-                <input 
-                  className="p-2 text-md w-full" 
-                  id="lastname" 
-                  type="text" 
-                  required
-                  value={lastName}
-                  onChange={e => {
-                    setLastName(e.target.value)
-                  }}
-                />
+
+              <p className='mt-4 mb-4'>Want to change your email? Contact the organizers at <a href='mailto:rythmhacks@gmail.com'>rythmhacks@gmail.com</a>.</p>
+
+              <button type="submit" disabled={
+                email.trim() === user?.email 
+                && firstName.trim() === (user?.user_metadata.first_name || "Unnamed")
+                && lastName.trim() === (user?.user_metadata.last_name || "Hacker")
+              }>Save</button>
+
+              <div 
+                className="px-4 py-2 mt-4 items-center bg-green-500/20 text-green-300 rounded-md"
+                style={{ display: nameUpdated ? "flex" : "none"}}
+              >
+                <BsFillCheckCircleFill size={24} fill="#33FF44" className="mr-4" />
+                <p><strong>Success!</strong> Your name has been updated.</p>
               </div>
-            </div>
-            <div className="mb-4">
-              <label className="block mb-2" htmlFor="email">Email</label>
-              <input
-                className="w-full p-2 text-md"
-                id="email"
-                type="email"
-                required
-                value={email}
-                onChange={e => {
-                  setEmail(e.target.value)
-                }}
-              />
-            </div>
 
-            <p className='mt-4 mb-4'>Want to change your email? Contact the organizers at <a href='mailto:rythmhacks@gmail.com'>rythmhacks@gmail.com</a>.</p>
-
-            <button type="submit" disabled={
-              email.trim() === user?.email 
-              && firstName.trim() === (user?.user_metadata.first_name || "Unnamed")
-              && lastName.trim() === (user?.user_metadata.last_name || "Hacker")
-            }>Save</button>
-
-            <div 
-              className="px-4 py-2 mt-4 items-center bg-green-500/20 text-green-300 rounded-md"
-              style={{ display: nameUpdated ? "flex" : "none"}}
-            >
-              <BsFillCheckCircleFill size={24} fill="#33FF44" className="mr-4" />
-              <p><strong>Success!</strong> Your name has been updated.</p>
-            </div>
-
-            {/* <div 
-              className="px-4 py-2 mt-4 items-center bg-yellow-500/20 text-yellow-200 rounded-md"
-              style={{ display: emailUpdated ? "flex" : "none"}}
-            >
-              <RiErrorWarningFill size={24} fill="#FFEE22" className="mr-4" />
-              <p>Check your email inbox at <span className="text-[#659CE1]">{email}</span> to finalize your email change.</p>
-            </div> */}
-          </form>
+              {/* <div 
+                className="px-4 py-2 mt-4 items-center bg-yellow-500/20 text-yellow-200 rounded-md"
+                style={{ display: emailUpdated ? "flex" : "none"}}
+              >
+                <RiErrorWarningFill size={24} fill="#FFEE22" className="mr-4" />
+                <p>Check your email inbox at <span className="text-[#659CE1]">{email}</span> to finalize your email change.</p>
+              </div> */}
+            </form>
+          </div>
+          <div className='container'>
+            <h2>Delete Account</h2>
+            <p>To request deletion your account, contact us at <a href='mailto:rythmhacks@gmail.com'>rythmhacks@gmail.com</a> and specify the email of your account.</p>
+          </div>
         </div>
         <div className='container mt-4'>
           <h1>Theme</h1>
@@ -149,17 +149,6 @@ const Settings = () => {
           <h1>Privacy</h1>
           <p>Here at RythmHacks, we highly value having a <Link to='/privacy'>privacy policy.</Link></p>
         </div>
-        {/* <div className='container mt-4'>
-          <h2>Delete Account</h2>
-          <p>Press the button below to delete your account. <strong>This cannot be undone.</strong></p>
-          <button className='!bg-red-600 hover:!bg-red-500 mt-4' onClick={
-            () => {
-              if (window.confirm('Are you sure you want to delete your account? This cannot be undone.')) {
-                deleteUser()
-              }
-            }
-          }>Delete my account</button>
-        </div> */}
       </div>
     )
   }
