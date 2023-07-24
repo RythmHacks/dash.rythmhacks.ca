7e2c7403d7186cf8c847b9426a6b6a9280ac9e16
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-07-24 13:11:12 -0400

    fix(apps): red dot next to save button

--- src/pages/Apply/HackerApplication.tsx
+++ src/pages/Apply/HackerApplication.tsx
@@ -460,7 +460,7 @@ const HackerApplication = () => {
 
             <div className='flex gap-2 mt-8'>
             <button className='contrast' onClick={() => navigate('/dashboard/register')}>Save and return</button>
-            <span id="rewardId" style={{width: 2, height: 2, background: "red"}} />
+            {applicationData.status !== 'Submitted' && <span id="rewardId" style={{width: 2, height: 2, background: "red"}} />}
             {applicationData.status !== 'Submitted' && <button type="submit" style={{backgroundColor: (submitted) ? "#64B786" : "#558CA9"}}>{(!submitted) ? "Submit (you can edit it later)" : "Submitted!"}</button>}
             </div>
             {validationMessages && (<div className="mt-4 flex-col text-left !items-start text-dark3" style={{display: (showValidationMessages ? "flex" : "none")}}>
