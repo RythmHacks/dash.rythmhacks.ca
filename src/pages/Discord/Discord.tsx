652fe149e80f0793e445d26a9bddc9847dbbd9cb
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-08-26 12:03:48 -0400

    fix(discord): don't care if you're already in the server

--- src/pages/Discord/Discord.tsx
+++ src/pages/Discord/Discord.tsx
@@ -13,13 +13,16 @@ const Discord = () => {
     const [result, setResult] = useState<object>()
     const [error, setError] = useState<Error>()
 
-    if (user?.user_metadata.joined_discord) {
-        return (
-            <div className="page">
-                Success, you've been added
-            </div>
-        )
-    }
+    // if (user?.user_metadata.joined_discord) {
+    //     return (
+    //         <div className="page">
+    //             <div className='container'>
+    //                 <h1>Success!</h1>
+    //                 <p>Success! You've been added to the Discord server. Now, head over to the Discord website or app and begin chatting!</p>
+    //             </div>
+    //         </div>
+    //     )
+    // }
 
     if (code) {
         useEffect(() => {
@@ -54,15 +57,21 @@ const Discord = () => {
         if (loadingState === 'loading') return <div className="lazy-preloader"></div>
         else if (loadingState === 'error') return (
             <div className="page">
-                <h1>An error occured and you could not have been added to the server!</h1>
-                <p>Message: {error!.message}</p>
+                <div className='container'>
+                    <h1>An error occured and you were not added to the server!</h1>
+                    <p>Message: {error!.message}</p>
+                    <p>You may already be in the server.</p>
+                    <p>Contact us at <a href='mailto:rythmhacks@gmail.com'>rythmhacks@gmail.com</a> for more assistance.</p>
+                </div>
 
-                <p>Contact the RythmHacks devs to fix the issue.</p>
             </div>
         )
         return (
             <div className="page">
-                Success, you've been added
+                <div className='container'>
+                    <h1>Success!</h1>
+                    <p>Success! You've been added to the Discord server. Now, head over to the Discord website or app and begin chatting!</p>
+                </div>
             </div>
         )
     }
@@ -70,17 +79,21 @@ const Discord = () => {
     else {
         const discordOAuth2Link = encodeURI(
             'https://discord.com/api/oauth2/authorize?'
-            + `client_id=${import.meta.env.VITE_DISCORD_CLIENT_ID}`
+            + `client_id=1086480799961976923`
             + `&redirect_uri=${window.location.href}`
             + '&response_type=code'
             + '&scope=identify guilds.join'
         )
 
-        return (<div className="page">
-            <div className='container w-full'>
+        return (<div className="page w-full">
+            <div className='container'>
                 <h1>Join the Discord</h1>
+                <p>Join the RythmHacks Discord server and start talking to your fellow hackers! If you've already joined the Discord, you don't need to do anything.</p>
+            </div>
+            <div className='container mt-4'>
+                <p>Press this button to authorize your Discord account and join the server. </p>
                 <a 
-                    className="style-none text-white bg-[#5865F2] px-4 py-2 rounded-md no-underline flex items-center gap-2 w-48 hover:bg-[#4f5bda]"
+                    className="style-none text-white bg-[#5865F2] px-4 py-2 rounded-md no-underline flex items-center gap-2 w-48 hover:bg-[#4f5bda] mt-4"
                     href={discordOAuth2Link}
                 ><BsDiscord size="24" />Join the Discord</a>
             </div>
