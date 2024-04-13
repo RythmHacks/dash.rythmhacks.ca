97b4f0daae0a1b9502ddfb786df85c105622ee03
Daniel Zhang <d.zhang200788@gmail.com>
2024-04-13 13:04:26 -0400

    fix: prisma singleton

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
