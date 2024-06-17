9e1c447bae3b2882a65ea5da80fbf996dfdde3f5
Daniel Zhang <d.zhang200788@gmail.com>
2024-06-16 23:25:17 -0400

    fix: prisma on the edge

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
