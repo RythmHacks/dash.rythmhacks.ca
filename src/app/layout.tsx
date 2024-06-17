344e37ec744d8a9f1defd8b9747d237fbb867186
Daniel Zhang <d.zhang200788@gmail.com>
2024-06-17 16:58:40 -0400

    Revert "fix: prisma on the edge"
    This reverts commit 9e1c447bae3b2882a65ea5da80fbf996dfdde3f5.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-export const runtime = "edge";
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./styles.scss";
--- src/prisma.ts
+++ src/prisma.ts
@@ -1,9 +1,8 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
