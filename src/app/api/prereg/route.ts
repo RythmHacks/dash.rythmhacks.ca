b92b4f2f0b00fdacc57936e8624143911b99d707
Daniel Zhang <d.zhang200788@gmail.com>
2024-06-22 14:41:52 -0400

    Danielz/tech 40 preregistration (#93)
    * feat: add api route
    
    * chore: hide dashboard
    
    * fix: flip direction of env
    
    * fix: error handling

--- src/app/api/prereg/route.ts
+++ src/app/api/prereg/route.ts
@@ -1,15 +1,29 @@
 import prisma from "@/prisma";
+import { Prisma } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (request: NextRequest) => {
-    await prisma.preregistration.create({ data: { email: (await request.json()).email } });
-    return new NextResponse("", {
-        status: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "https://rythmhacks.ca",
-            "Access-Control-Allow-Headers": "Content-Type",
-        },
-    });
+    try {
+        await prisma.preregistration.create({ data: { email: (await request.json()).email } });
+
+        return new NextResponse("", {
+            status: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "https://rythmhacks.ca",
+                "Access-Control-Allow-Headers": "Content-Type",
+            },
+        });
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === "P2002") {
+            return new NextResponse("email already exists", {
+                status: 400,
+                headers: {
+                    "Access-Control-Allow-Origin": "https://rythmhacks.ca",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                },
+            });
+        }
+    }
 };
 
 export const OPTIONS = () => {
